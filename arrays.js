3e21260702720a31dd77c00aaa48f2c53954ee5b
IcaroAugusto <icaro10100@hotmail.com>
2020-01-29 22:49:49 -0300

    Shuffle

--- arrays.js
+++ arrays.js
@@ -15,3 +15,25 @@ Array.prototype.pick = function() {
 Array.prototype.last = function() {
   return this[this.length-1];
 }
+
+//Shuffles the values of the array
+Array.prototype.shuffle = function() {
+  var currentIndex = this.length;
+  var temporaryValue;
+  var randomIndex;
+  while (0 !== currentIndex) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = this[currentIndex];
+    this[currentIndex] = this[randomIndex];
+    this[randomIndex] = temporaryValue;
+  }
+  return this;
+}
+
+//Returns a copy of the array with shuffled elements
+Array.prototype.shuffled = function() {
+  var result = this.slice();
+  result.shuffle();
+  return result;
+}
